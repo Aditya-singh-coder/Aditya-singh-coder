891760c666d5e757e9f7bd1ab84b217ee525ccd0
Aditya singh <lokeshlokesh9308@gmail.com>
2023-09-01 10:53:54 +0530

    updated

---
+++ Project/project.cpp
@@ -0,0 +1,20 @@
+/* ek aisa notebook jo ki coders ke liye bana hai. waha pe aap code, video link, photo,GIF,pdf, kuch bhi attach kar sakte hai.
+(1)universitites,companies aur bhi kuch hai to wo bhi uspar apna important file with code daal sakte hai.
+(2)VScode se, github se ya kahi se bhi code ko import,export kar sakte hai.
+(3)paise kamane ke tarike- Ai features, pdf scan karne ka feature etc. premium subscription par milega.
+(4)koi agar chahe to apna note share bhi kar sakta hai. wo chahe to us note ko lock bhi kar sai. jab tak koi paisa na de tab tak.
+(5)aap code ke andar bhi writing style wale editing kar sakte hai.
+(6)sayad mujhe premium me bhi teirs banae pade.
+(7)premium me ek aur chij dali jayegi. usme aap kisi video ka subtitle scan karke usko notebbok me paste kar sakte hai.
+(8) video se liye hue har subtitle ke sath uska timestap rahega.
+(9) agar app par video dala gaya hai to video me jis point par IDE me jo dala gaya hai wo hi apke ek special file me update hoti rahegi.
+(10) coding related questions app pe live puch sakte hai. just like filo.
+(11) coding related discussions ho sakta hai. just like stack overflow.
+(12) number (9) wala feature live videos me bhi rahega.
+(13) agar bacha chahe to code edit kar sakta hai live video me. isse discussion karne me aur jayada madad ho jayegi.
+(14) bache ke edit karne ka allow sirf teacher kar sakta hai.
+(15) agar apne discussion me question bataye to apko points milega. ek limit tak point ho jane ke baad aap usse premium free me le sakte hai.
+(16) pdf scan karke dalne ka bhi feature hoga jo ki free hoga.
+(17) pdf scan karte waqt agar koi picture ho to wo bhi scan kar sakte hai.
+(18)udemy ki tarah coding cousrses bhi daal payenge.
+*/
---
+++ Project/things_i_have_to_learn_for_project.cpp
@@ -0,0 +1,29 @@
+/*
+Aapke project ko puri tarah se banaane ke liye kuch mukhya programming skills ki avashyakta hogi. Yahan kuch aise skills hain jo aapko madad kar sakte hain:
+
+1. **Web Development**: Aapko web application banani hogi, iske liye aapko front-end aur back-end development ki samajh honi chahiye. HTML, CSS, JavaScript, aur ek back-end language (jaise ki Python, Node.js, PHP, ya Ruby) aapko sikhni padegi.
+
+2. **Database Management**: User data, notes, files, discussions, etc. ke liye aapko ek database ki zaroorat hogi. SQL (ya NoSQL) databases ki basic understanding honi chahiye.
+
+3. **Version Control (Git)**: Aapke project ke code ko track karne aur manage karne ke liye Git ka istemal karna aavashyak hai.
+
+4. **API Integration**: Aapke platform mein kuch third-party services aur APIs bhi shamil honge, jaise ki video subtitle scanning. Iske liye API integration ki samajh honi chahiye.
+
+5. **Security Best Practices**: Kyunki aap user data aur files ko handle kar rahe hain, aapko security best practices ki samajh honi chahiye taki user data surakshit rahe.
+
+6. **AI and Machine Learning (Optional)**: Agar aap AI features jaise ki writing style editing implement karna chahte hain, to machine learning aur natural language processing (NLP) ki samajh honi chahiye.
+
+7. **Payment Gateway Integration**: Premium subscription ke liye payment gateways ka istemal karna hoga, isliye unki integration ki samajh honi chahiye.
+
+8. **User Interface (UI) and User Experience (UX) Design**: Aapke platform ka UI/UX user-friendly aur visually appealing hona chahiye. Basic UI/UX design principles ki samajh honi chahiye.
+
+9. **Server Management and Deployment**: Aapke application ko live karne ke liye server management aur deployment ki samajh honi chahiye.
+
+10. **Testing and Debugging**: Aapke project ko thoroughly test karna aur bugs ko debug karna bhi mahatvapurn hai. Testing frameworks aur debugging tools ki samajh honi chahiye.
+
+11. **Collaboration Tools**: Agar aapke sath koi aur developer ya team hai, to collaboration tools (jaise ki Slack, Trello, etc.) ki samajh honi chahiye.
+
+12. **Documentation**: Apne code, APIs, aur features ko samjhane ke liye achhi documentation likhna bhi mahatvapurn hai.
+
+Yeh skills aapke project ko safalta se banaane mein madad karenge. Yadi aap in skills ko sikhna shuru karte hain aur prashikshan prapt karte hain, to aap apne project ko achhi tarah se develop kar sakte hain. Samay aur prashikshan se, aap apne uddeshya tak pahunch sakte hain.
+*/
