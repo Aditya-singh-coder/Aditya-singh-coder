221fbb690cef726edb0dcab67ad26099e4f82564
Aditya singh <lokeshlokesh9308@gmail.com>
2023-09-13 20:39:43 +0530

    updated

--- learning_C++/4_sorting/1_search_sorting/1_selection_sorting.cpp
+++ learning_C++/4_sorting/1_search_sorting/1_selection_sorting.cpp
@@ -5,7 +5,7 @@ int main() {
     cout << "enter the numbers you wish to sort " << endl;
 
     for(int i=0; i<7; i++) {
-    cin >> arr[i]; 
+    cin >> arr[i];
     }
     cout << " the sorted numbers are: " <<endl;
     for(int a=0; a<7; a++) {
