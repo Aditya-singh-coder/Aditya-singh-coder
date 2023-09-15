23f036440134cf2095fb4eeb08d96785e892b2eb
Aditya singh <lokeshlokesh9308@gmail.com>
2023-09-15 21:55:45 +0530

    updated

---
+++ learning_C++/4_sorting/3_bubble_sorting/bubble_sorting.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int n=7;
+    int arr[n] = {1,32,7,44,36,2,6};
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j=0; j<n-1-i; j++) 
+        {
+            if(arr[j]>arr[j+1]) 
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    cout << "sorted array is ";
+    for( int k=0; k<n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+    return 0;
+}
