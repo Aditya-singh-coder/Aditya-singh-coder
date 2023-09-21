6d26dd754ba3d17f1449fbf2724cf447a6bf6086
Aditya singh <lokeshlokesh9308@gmail.com>
2023-09-21 22:25:58 +0530

    updated

--- learning_C++/4_sorting/4_1_slection_sorting/1_selection_sorting.cpp
+++ learning_C++/4_sorting/4_1_slection_sorting/1_selection_sorting.cpp
@@ -9,13 +9,12 @@ int main() {
     }
     cout << " the sorted numbers are: " <<endl;
     for(int a=0; a<7; a++) {
-        int minindex = arr[a];
         for(int b=a+1; b<7; b++) {
             if( arr[b] < arr[a]) {
-                swap( minindex, arr[b]);
+                swap( arr[a], arr[b]);
             }
         }
-        cout << minindex << " ";
+        cout <<arr[a] << " ";
     }
     return 0;
 }
